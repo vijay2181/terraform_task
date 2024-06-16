79c8ff2c87756142a4e92d77e1afd541ea43efb3
Vijay Kumar <66196388+vijay2181@users.noreply.github.com>
2024-06-16 23:38:16 +0530

    Create security-groups.tf

---
+++ VPC/security-groups.tf
@@ -0,0 +1,62 @@
+# Security Group for Public Instances
+resource "aws_security_group" "public-SG" {
+  name        = "myvpc-public-SG"
+  description = "VPC public Security Group"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# Security Group for Load Balancer
+resource "aws_security_group" "lb_sg" {
+  name        = "myvpc-lb-SG"
+  description = "VPC Load Balancer Security Group"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
